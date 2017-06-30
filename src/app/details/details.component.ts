eaea7592c6501d642bd00fbbda6f9af7ac1ab2bf
dfuentes22 <dvf5104@gmail.com>
2017-06-30 11:45:21 -0400

    added details

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -17,7 +17,8 @@ export class DetailsComponent implements OnInit {
 
   onToggleDetails() {
   	this.isVisible = !this.isVisible;
-  	this.log.push(this.log.length + 1);
+  	// this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 
 }
